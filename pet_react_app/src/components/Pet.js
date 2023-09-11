f0e9ee39f3172b62673b6d1a3db57c916823fa64
Abdulaziz-MI <am.alzaid@outlook.com>
2023-09-11 17:39:06 +0100

    created the fetch userdata function

--- pet_react_app/src/components/Pet.js
+++ pet_react_app/src/components/Pet.js
@@ -1,6 +1,15 @@
-const Pet = () =>{
+const Pet = ({ loggedInUser }) => {
+    if (!loggedInUser || !loggedInUser.pets || loggedInUser.pets.length === 0) {
+      return <h2>No pets available</h2>;
+    }
+  
+    return (
+      <>
+        <h2>{loggedInUser.pets[0].name}</h2>
+        <h3> {loggedInUser.pets[0].species}</h3>
 
-
-}
-
-export default Pet;
+      </>
+    );
+  };
+  
+  export default Pet;
--- pet_react_app/src/containers/PetContainer.js
+++ pet_react_app/src/containers/PetContainer.js
@@ -1,5 +1,32 @@
 import { useEffect, useState } from "react";
+import Pet from "../components/Pet";
 const PetContainer = () => {
+
+const [loggedInUser, setLoggedInUser] = useState([])
+
+const fetchUserData = async () => {
+    const response = await fetch("http://localhost:8080/users/1");
+    const data = await response.json();
+    setLoggedInUser(data)
+
+}
+
+
+useEffect( () => {
+   
+    fetchUserData()
+    console.log(loggedInUser);
+
+},[])
+
+
+
+   return(<>
+        <Pet loggedInUser={loggedInUser}/>
+   </>)
+
+
+
 
 }
 
