3a77ebc4209916c6afd98bee13ec880b96b46643
Jannah Anwar <jannah.anwar@outlok.com>
2023-09-12 17:40:00 +0100

    started implementing react router

--- pet_react_app/src/App.js
+++ pet_react_app/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import PetContainer from './containers/PetContainer';
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import PetProfile from './components/PetProfile';
 
 function App() {
+  const [pets, setPets] = useState([]);
   return (
     <>
-    <h2>Pet title</h2>
-    <PetContainer />
+    <h2>Virtual Pet</h2>
+    {/* <PetContainer /> */}
+    <BrowserRouter>
+    <div className="App">
+        {/* <NavLink to="/">Home</NavLink> */}
+        <Routes>
+          <Route index element={<PetContainer pets={pets}/>}></Route>
+          <Route path='/pets/:id' element={<PetProfile pets={pets}/>}></Route>
+        </Routes>
+    </div>
+    </BrowserRouter>
     </>
   );
 }
-
 export default App;
