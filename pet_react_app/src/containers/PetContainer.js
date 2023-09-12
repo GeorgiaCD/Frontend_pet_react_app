076ec228ccf7721ddb24335bd22968dbba4ae1c7
Abdulaziz-MI <am.alzaid@outlook.com>
2023-09-12 16:51:29 +0100

    update pet in progress

--- pet_react_app/src/containers/PetContainer.js
+++ pet_react_app/src/containers/PetContainer.js
@@ -3,7 +3,10 @@ import Pet from "../components/Pet";
 import UserPetList from "../components/UserPetList";
 const PetContainer = () => {
 
+// const [loggedInUserPets, setLoggedInUserPets] = useState({})
+
 const [loggedInUser, setLoggedInUser] = useState([])
+
 
 const fetchUserData = async () => {
     const response = await fetch("http://localhost:8080/users/5");
@@ -12,14 +15,35 @@ const fetchUserData = async () => {
 
 }
 
+const updatePet = (fedPet) => {
+
+   const copiedUser = loggedInUser 
+
+   for (let index = 0; index < copiedUser.pets.length; index++) {
+    
+    if(fedPet.id === copiedUser.pets[index].id){
+        copiedUser.pets.splice(index, 1, fedPet)
+    }
+  
+   }
+   setLoggedInUser(copiedUser)
+   console.log( "copied user: ", copiedUser);
+
+    
+}
+
 const feedPet = async (petId) => {
         const response = await fetch(`http://localhost:8080/pets/${petId}/feed?foodId=1`, {
           method: 'PATCH',
           headers: {
             'Content-Type': 'application/json',
-            
+
           },
-        });}
+        })
+        const data = await response.json()
+            console.log("data: ", data);
+        updatePet(data.body);
+        } 
 
 
 useEffect( () => {
