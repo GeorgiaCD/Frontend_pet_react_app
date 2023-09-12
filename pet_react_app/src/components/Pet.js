dfdc932dc6cce5fd31d70b80f44f26ffc6d81ef3
GeorgiaCD <georgiacrawforddellar@gmail.com>
2023-09-12 10:27:31 +0100

    added pet properties to pet component

--- pet_react_app/src/components/Pet.js
+++ pet_react_app/src/components/Pet.js
@@ -4,12 +4,24 @@ const Pet = ({ loggedInUser }) => {
     }
   
     return (
-      <>
-      
+      <div className="Pet">
         <h2>{loggedInUser.pets[0].name}</h2>
         <h3> {loggedInUser.pets[0].species}</h3>
-
-      </>
+        <p>Happiness level: {loggedInUser.pets[0].happinessLevel}/100</p>
+        <p>Energy level: {loggedInUser.pets[0].energyLevel}/100</p>
+        <ul>
+            <h3>Toys</h3>
+            {loggedInUser.pets[0].toys.map((toy, index) => (
+              <li key={index}>{toy.name} - {toy.happinessValue}</li>
+            ))}
+        </ul>
+        <ul>
+            <h3>Foods</h3>
+            {loggedInUser.pets[0].foods.map((food, index) => (
+              <li key={index}>{food.name} - {food.nutritionValue}</li>
+            ))}
+        </ul>
+      </div>
     );
   };
   
